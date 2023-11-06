036cafb4d73d4213fbb18d24e58946392591ddee
hyunmo <jrsky723@gmail.com>
2023-11-06 14:04:18 +0900

    Initialize project using Create React App
    - Create React App
    - delete all previous works

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebKakaoTalk
-Cloning Kakao Talk in Web (실시간 채팅 구현)
--- src/controllers/chatController.js
+++
@@ -1,4 +0,0 @@
-export const see = (req, res) => res.render("chat");
-export const search = (req, res) => res.send("Search");
-export const create = (req, res) => res.send("Create");
-export const remove = (req, res) => res.send("Remove");
