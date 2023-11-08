2261da05024338da3cb37d84d636ba8bf7528672
wonduck-hub <dnjsejr417@naver.com>
2023-11-08 16:18:55 +0900

    socket.io를 사용한 패킷 전달 확인
    'http://localhost:4000/chats/create'에서 전송한 내용을 server.js에서 받는 것을 확인했다.

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import chatsRouter from "./routers/chatsRouter";
+import http from "http";
+import socketIO from "socket.io";
+import { disconnect } from "process";
 
 const PORT = 4000;
 
@@ -11,6 +14,9 @@ console.log(process.cwd());
 const app = express();
 const logger = morgan("dev");
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
@@ -18,7 +24,19 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/chats", chatsRouter);
 
+// socket.io를 통한 패킷 교환
+io.on("connection", (socket)=>{
+  socket.on("chatting", (data)=>{
+    console.log(data);
+    //여기에 들어오는 패킷을 채팅들이다
+  });
+  socket.on("disconnect", (data) =>{
+    console.log("disconnect");
+  })
+});
+
+
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening);
+server.listen(PORT, handleListening);
