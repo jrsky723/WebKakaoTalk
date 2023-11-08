7a6fbf62b8bb1ec33612138683c69a46ae441224
wonduck-hub <dnjsejr417@naver.com>
2023-11-08 16:32:18 +0900

    이전 커밋 내용 수정
    create는 채팅방 생성이므로 임시로 패킷을 보내는 경로 temp를 설정

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -12,8 +12,10 @@ export const rooms = (req, res) => {
 };
 export const see = (req, res) => res.render("chat");
 export const search = (req, res) => res.send("Search");
-export const create = (req, res) => res.send(inputChatHTML);
+export const create = (req, res) => res.send("Create");
 export const remove = (req, res) => res.send("Remove");
+
+export const temp = (req, res) => res.send(inputChatHTML);
 
 // socket.io 패킷 교환 확인용 임시 변수
 const inputChatHTML = `<!DOCTYPE html>
