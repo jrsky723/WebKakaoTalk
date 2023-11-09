cd949e91a47511e1fec4e2ffc55910d277e20dfa
wonduck-hub <dnjsejr417@naver.com>
2023-11-09 09:52:56 +0900

    임시 html을 pug로 변경

--- src/routers/chatsRouter.js
+++ src/routers/chatsRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { see, create, search, remove } from "../controllers/chatController";
+import { see, create, search, remove, temp } from "../controllers/chatController";
 
 const chatsRouter = express.Router();
 
@@ -7,5 +7,6 @@ chatsRouter.get("/:id(\\d+)", see);
 chatsRouter.get("/:id(\\d+)/remove", remove);
 chatsRouter.get("/create", create);
 chatsRouter.get("/search", search);
+chatsRouter.get("/temp", temp);
 
 export default chatsRouter;
