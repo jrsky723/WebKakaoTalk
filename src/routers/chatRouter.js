662cd08e34099aeda935603609385187fe6fa046
hyunmo <jrsky723@gmail.com>
2023-11-10 17:39:34 +0900

    5.0 Create Room & Add Array
    - DB 연결 전, 채팅방을 만들고 Array에 넣기

---
+++ src/routers/chatRouter.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  see,
+  getEdit,
+  postEdit,
+  getCreate,
+  postCreate,
+} from "../controllers/chatController";
+
+const chatRouter = express.Router();
+
+chatRouter.get("/:id(\\d+)", see);
+chatRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+chatRouter.route("/create").get(getCreate).post(postCreate);
+
+export default chatRouter;
--- src/server.js
+++ src/server.js
@@ -2,11 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
-import chatsRouter from "./routers/chatsRouter";
+import chatRouter from "./routers/chatRouter";
 
 const PORT = 4000;
-
-console.log(process.cwd());
 
 const app = express();
 const logger = morgan("dev");
@@ -14,9 +12,10 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
-app.use("/chats", chatsRouter);
+app.use("/chats", chatRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
